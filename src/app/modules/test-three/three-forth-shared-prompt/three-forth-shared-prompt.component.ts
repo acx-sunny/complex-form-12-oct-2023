2a30f060a9133a2b099346517c3cf9fd3d6d8941
acx-sunny <sunny.kumar@acxhange.com>
2023-10-12 16:19:10 +0530

    four

--- src/app/modules/test-three/three-forth-shared-prompt/three-forth-shared-prompt.component.ts
+++ src/app/modules/test-three/three-forth-shared-prompt/three-forth-shared-prompt.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { PromptInterface } from 'src/app/interfaces/prompt.interface';
+import { PromptsService } from 'src/app/services/prompts/prompts.service';
 
 @Component({
   selector: 'app-three-forth-shared-prompt',
   templateUrl: './three-forth-shared-prompt.component.html',
-  styleUrls: ['./three-forth-shared-prompt.component.css']
+  styleUrls: ['./three-forth-shared-prompt.component.css'],
 })
 export class ThreeForthSharedPromptComponent {
+  constructor(private _promptService: PromptsService) {}
 
+  promptWholeData: PromptInterface[] = [];
+
+  ngOnInit(): void {
+    this._promptService
+      .getPromptData()
+      .subscribe((response: PromptInterface[]) => {
+        this.promptWholeData = response;
+      });
+  }
 }
