891de9c8a3da29d44e218c5b1a290268f2dc4a4c
acx-sunny <sunny.kumar@acxhange.com>
2023-10-12 16:29:12 +0530

    six

--- src/app/modules/test-three/three-sixth-shared-script/three-sixth-shared-script.component.ts
+++ src/app/modules/test-three/three-sixth-shared-script/three-sixth-shared-script.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ScriptInterface } from 'src/app/interfaces/script.interface';
+import { ScriptsService } from 'src/app/services/scripts/scripts.service';
 
 @Component({
   selector: 'app-three-sixth-shared-script',
   templateUrl: './three-sixth-shared-script.component.html',
-  styleUrls: ['./three-sixth-shared-script.component.css']
+  styleUrls: ['./three-sixth-shared-script.component.css'],
 })
 export class ThreeSixthSharedScriptComponent {
+  constructor(private _scriptService: ScriptsService) {}
 
+  scriptWholeData: ScriptInterface[] = [];
+
+  ngOnInit(): void {
+    this._scriptService
+      .getscriptData()
+      .subscribe((response: ScriptInterface[]) => {
+        this.scriptWholeData = response;
+      });
+  }
 }
